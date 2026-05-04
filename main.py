b4518fffdbeafe49c04d5e55efa0b9f6f38f6017
viniciusccosta <viniciusccosta95@gmail.com>
2026-05-04 01:07:07 -0300

    Add IP & MAC page: Implement file upload, data filtering, and visualization features

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Nettrace", layout="wide")
 pages = [
     st.Page("pages/IP.py", title="IP"),
     st.Page("pages/MAC.py", title="MAC"),
+    st.Page("pages/IP_MAC.py", title="IP & MAC"),
 ]
 
 navigation = st.navigation(pages)
